28ad0380439b84784655c59d522544cf3fabac33
Josep Fanals i Batllori <jfanals@eroots.tech>
2023-11-07 10:04:27 +0100

    Write on the optimization formulation

--- doc/rst_source/candidates_optimization/load_and_scale.py
+++
@@ -1,7 +0,0 @@
-from GridCalEngine import *
-from GridCalEngine.IO.file_handler import FileOpen, FileSave
-
-
-grid = FileOpen('Spain.gridcal').open()
-# The ieee118 .gridcal file may be damaged, redo or pick another grid
-print('Done')
